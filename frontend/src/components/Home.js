5825a4c5c24771e8df5f7dc09a6490b5e6cda52b
cristina gradinaru <cristinagradinaru90@gmail.com>
2021-05-08 11:14:45 -0500

    third

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import JoinRoom from "./JoinRoom";
 import CreateRoom from "./CreateRoom";
+import Room from "./Room";
 import {BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom'
 
 export default class Home extends Component {
@@ -15,6 +16,7 @@ export default class Home extends Component {
                     <Route exact path='/'><p>This is the home page</p></Route>
                     <Route exact path='/join' component={JoinRoom}></Route>
                     <Route exact path='/create' component={CreateRoom}></Route>
+                    <Route exact path='/room/:roomCode' component={Room}></Route>
                 </Switch>
             </Router>
         )
--- frontend/src/components/JoinRoom.js
+++ frontend/src/components/JoinRoom.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+import {TextField, Button, Grid, Typography } from '@material-ui/core';
+import {Link} from 'react-router-dom';
 
 export default class JoinRoom extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            roomCode: "",
+            error: " ",
+        }
+    }
     render() {
         return (
-            <div>
-                This is the JoinRoom page
-            </div>
+            <Grid container spacing={1} align="center">
+                <Grid item xs={12} align="center"> 
+                    <Typography variant='h4' component = 'h4'>
+                        Join a Room
+                    </Typography>
+                </Grid>
+                <Grid xs={12} align="center">
+                    <TextField error= {this.state.error} label='Code' placeholder='Enter a Room Code' value={this.state.roomCode } helperText={this.state.error} variant = "outlined"/>
+                </Grid>
+
+            </Grid>
         )
     }
 }
